4cf64fdda725438ac37ff10a37d7c859e616fd9c
eugen_zha <eugen.zha@gmail.com>
2019-04-09 17:37:10 +0300

    Initial commit

---
+++ src/app-routes.js
@@ -0,0 +1,16 @@
+import { HomePage, DisplayDataPage, ProfilePage } from './pages';
+
+export default [
+  {
+    path: '/display-data',
+    component: DisplayDataPage
+  },
+  {
+    path: '/profile',
+    component: ProfilePage
+  },
+  {
+    path: '/home',
+    component: HomePage
+  }
+];
