7123889b1ec6df14143e98a729c026a9eb78cc9d
Arman Jivanyan <arman.jivanyan@devexpress.com>
2025-09-10 10:53:53 +0400

    Revert "CI: Fix vite build paths and migrate to npm ci (#63)"
    This reverts commit 1b87e08cd52706662a19da7ab10f79946fa37cc2.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/devextreme-react-template/',
   plugins: [react()],
 })
